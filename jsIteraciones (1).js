c0a05b5f8abbc76109d0fe4d024a1cf3934b431f
FMPower <60629415+FMPower@users.noreply.github.com>
2020-02-17 20:27:24 -0300

    Add files via upload

---
+++ jsIteraciones (1).js
@@ -0,0 +1,13 @@
+function mostrar()
+{
+	var contador;
+	contador = 0;
+	
+	while(contador <10){
+		++contador;
+		
+		alert(contador);
+
+}
+
+}//FIN DE LA FUNCIÓN
---
+++ jsIteraciones (2).js
@@ -0,0 +1,12 @@
+function mostrar()
+{
+	var contador;
+	contador = 10;
+
+	while(contador >1){
+		--contador;
+
+		alert(contador);
+}
+
+}//FIN DE LA FUNCIÓN
