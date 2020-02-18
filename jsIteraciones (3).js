f72d4ccd10cfece9c27625753a855d5c64be42f4
FMPower <60629415+FMPower@users.noreply.github.com>
2020-02-17 21:04:56 -0300

    Add files via upload

---
+++ jsIteraciones (3).js
@@ -0,0 +1,17 @@
+function mostrar()
+{
+    var clave = prompt("Ingrese el número clave.");
+    var contador;
+
+    while(clave |= "utn750"){
+        if(contador<3){
+            clave = prompt("Ingrese su clave.");    
+        }
+        break;
+
+}
+    if(clave == "utn750"){
+        alert("Bienbenido.");
+    }
+    
+}//FIN DE LA FUNCIÓN
